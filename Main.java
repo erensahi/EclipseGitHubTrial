0eb56c737b20721ba14b15781cc4043471423d95
erensahi <113985249+erensahi@users.noreply.github.com>
2022-09-28 15:12:21 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,27 @@
-package arkadasSayilar;
+package sayiBulma;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// 220 ve 284 arkadaş sayılardır
 		
-		int a=220;
-		int b=284;
-		int sumA=0;
-		int sumB=0;
-		
-	for (int i=1;i<a;i++) {
-		if (a%i==0) {
-			sumA=sumA+i;
-		}
-	}
-	
-	for (int j=1;j<b;j++) {
-		if (b%j==0) {
-			sumB=sumB+j;
-		}
-	}
-    if (sumA==b && sumB==a) {
-    	System.out.println("Bu sayılar arkadaş sayılardır.");
-    }
-    else {
-    	System.out.println("Bu sayılar arkadaş sayılar değildir.");
-    }
-    	
+		// int [] sayi = new int [] {1,2,4,6,7,8,9};
+		int [] sayi = {1,2,3,5,7,8,9,76};
+        int aranacakSayi =11;
+       boolean k=false;
+        
+        for(int deger: sayi){
+        	if (deger==aranacakSayi) {
+        		k=true;
+        	}
+        }
+        
+        if (k) {
+        	System.out.println("Aranan sayı dizide bulunmaktadır.");
+        }
+        else {
+        	System.out.println("Aranan sayı dizide bulunmamaktadır");
+        }
+        
 	}
 
 }
